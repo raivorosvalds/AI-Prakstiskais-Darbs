ddbf34c02faedc0d3adbdd37d0becaf48700202c
raivorosvalds <raivo.rosvalds@edu.rtu.lv>
2025-03-11 15:00:28 +0200

    Improvements to GUI

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,10 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
 import java.util.Random;
+import java.util.ArrayList;
 
-/*TODO: Pievienot iespeju izveleties skaitlus kas ir blakus viens otram
- * virknes garuma izveli(jslider?)
- */
 public class MainWindow extends JFrame {
     private JTable table;
     private JMenu turnMenu;
@@ -13,11 +11,11 @@ public class MainWindow extends JFrame {
     private JRadioButtonMenuItem player2;
     private JRadioButtonMenuItem minimax;
     private JRadioButtonMenuItem alphabeta;
+    private JSlider slider;
 
     public MainWindow() {
         // Tiek definets JFrame nav vajadzibas izmantot mainigo kurs ir JFrame objekts, jo tiek extends JFrame
         setTitle("AI Spele");
-        setSize(500, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
         // Tiek definets Menu bar prieks algoritma un gajiena izveles, abos default izvele ir "Speletajs" un "Minimax"
@@ -40,18 +38,44 @@ public class MainWindow extends JFrame {
         minimax.setSelected(true);
         algorithmMenu.add(minimax);
         algorithmMenu.add(alphabeta);
-        /*Poga prieks speles sakuma, ideja ir velak izdarit lai pec pogas nospiesanas start poga pazud un paradas poga turpinat
-         vinas vieta kuru speletajs nospiez lai varetu turpinat pec 2 ciparu izveles
-         */
+        slider = new JSlider(JSlider.HORIZONTAL, 15, 25, 15);
+        slider.setMajorTickSpacing(1);
+        slider.setMinorTickSpacing(1);
+        slider.setPaintTicks(true);
+        slider.setPaintLabels(true);
+        add(slider, BorderLayout.NORTH);
         JButton startButton = new JButton("Start");
         startButton.addActionListener(e -> {
+            // Debug
+            System.out.println(getSelectedAlgorithm());
+            System.out.println(getSelectedPlayer());
             System.out.println("Start button clicked!");
-            int[] array = generateArray(15);
-            String arrayText = "Skaitlu virkne: " + java.util.Arrays.toString(array);
-            JLabel arrayLabel = new JLabel(arrayText);
-            JPanel arrayPanel = new JPanel();
-            arrayPanel.add(arrayLabel);
-            add(arrayPanel, BorderLayout.NORTH);
+            ArrayList<Integer> array = generateArray(slider.getValue());
+            remove(slider);
+            revalidate();
+            repaint();
+            startButton.setVisible(false);
+            GameButtons buttons = new GameButtons(array);
+            JButton continueButton = new JButton("Turpinat");
+            continueButton.addActionListener(event -> {
+                if (!buttons.check()) {
+                    JOptionPane.showMessageDialog(this, "Izvēlētie cipari nav blakus!!", "Error", JOptionPane.ERROR_MESSAGE);
+                    return;
+                }
+                array.remove(0); // Placeholder
+                System.out.println(array); // Debug
+                buttons.updateButtons(array);
+                if (array.size() == 1) { // Placeholder
+                    remove(continueButton);
+                    continueButton.setVisible(false);
+                    buttons.dispose();
+                    startButton.setVisible(true);
+                    add(slider, BorderLayout.NORTH);
+                    revalidate();
+                    repaint();
+                }
+            });
+            add(continueButton, BorderLayout.SOUTH);
             revalidate();
             repaint();
         });
@@ -59,7 +83,7 @@ public class MainWindow extends JFrame {
         menuBar.add(algorithmMenu);
         setJMenuBar(menuBar);
         // Table kas paradis speles vesturi ieklauj Speletaja punktus ta briza virkni un Datora punktus
-        String[] columnNames = {"Speletajs", "Virkne", "Dators"};
+        String[] columnNames = {"Speletajs", "Dators"};
         Object[][] data = {};
         table = new JTable(data, columnNames);
         table.getTableHeader().setReorderingAllowed(false);
@@ -67,13 +91,31 @@ public class MainWindow extends JFrame {
         panel.add(new JScrollPane(table), BorderLayout.CENTER);
         panel.add(startButton, BorderLayout.SOUTH);
         add(panel, BorderLayout.CENTER);
+        pack();
+        setLocationRelativeTo(null);
         setVisible(true);
     }
-    protected int[] generateArray(int size) {
+    public JRadioButtonMenuItem getSelectedPlayer() {
+    if (player1.isSelected()) {
+        return player1;
+    } else if (player2.isSelected()) {
+        return player2;
+    }
+    return null;
+    }
+    public JRadioButtonMenuItem getSelectedAlgorithm() {
+        if (minimax.isSelected()) {
+            return minimax;
+        } else if (alphabeta.isSelected()) {
+            return alphabeta;
+        }
+        return null;
+    }
+    protected ArrayList<Integer> generateArray(int size) {
         Random rand = new Random();
-        int[] array = new int[size];
+        ArrayList<Integer> array = new ArrayList<>();
         for (int i = 0; i < size; i++) {
-            array[i] = rand.nextInt(9) + 1;
+            array.add(i, rand.nextInt(9) + 1);
         }
         return array;
     }
